2c96ef48103333df1f4313652d2f5e876039f59c
BugHunter-X <askzenai@gmail.com>
2025-02-05 23:00:06 +0100

    Add server.js file

---
+++ server.js
@@ -0,0 +1,11 @@
+const http = require('http');
+
+const requestListener = (request, response) => {
+  response.writeHead(200);
+  response.end('Hello, World!');
+};
+
+const server = http.createServer(requestListener);
+
+server.listen(8080);
+console.log('Server started on port 8080');
